ae0b8dbc4b0d0f13218ad425b02bad4a2474485e
Juankart65 <104481229+Juankart65@users.noreply.github.com>
2023-08-24 11:58:37 -0500

    Se agregó el CRUD de reserva

--- RecepcionAppV1/src/model/Hotel.java
+++ RecepcionAppV1/src/model/Hotel.java
@@ -2,13 +2,20 @@ package model;
 
 import java.util.ArrayList;
 
+import exceptions.ReservaException;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Alert.AlertType;
+
 public class Hotel {
 
 	private String nombre;
 
 	private ArrayList<Habitacion> listaHabitaciones = new ArrayList<>();
-	private ArrayList<Habitacion> listaReservas = new ArrayList<>();
-	private ArrayList<Habitacion> listaUsuarios = new ArrayList<>();
+	private ArrayList<Reserva> listaReservas = new ArrayList<>();
+	private ArrayList<Usuario> listaUsuarios = new ArrayList<>();
+	private Alert alert;
+	private DialogPane dialogPane;
 
 	public String getNombre() {
 		return nombre;
@@ -26,101 +33,191 @@ public class Hotel {
 		this.listaHabitaciones = listaHabitaciones;
 	}
 
-	public ArrayList<Habitacion> getListaReservas() {
+	public ArrayList<Reserva> getListaReservas() {
 		return listaReservas;
 	}
 
-	public void setListaReservas(ArrayList<Habitacion> listaReservas) {
+	public void setListaReservas(ArrayList<Reserva> listaReservas) {
 		this.listaReservas = listaReservas;
 	}
 
-	public ArrayList<Habitacion> getListaUsuarios() {
+	public ArrayList<Usuario> getListaUsuarios() {
 		return listaUsuarios;
 	}
 
-	public void setListaUsuarios(ArrayList<Habitacion> listaUsuarios) {
+	public void setListaUsuarios(ArrayList<Usuario> listaUsuarios) {
 		this.listaUsuarios = listaUsuarios;
 	}
 
 	public Hotel() {
 		super();
 	}
-	
-	
-	public Habitacion crearHabitacion (String id, ArrayList<Cama>listaCamas, Estado estado, Disponibilidad disponibilidad, TipoHabitacion tipoHabitacion) throws Exception {
-		
+
+	// -------------CRUD Habitacion-------------------------------------------------
+
+	public Habitacion crearHabitacion(String id, ArrayList<Cama> listaCamas, Estado estado,
+			Disponibilidad disponibilidad, TipoHabitacion tipoHabitacion) throws Exception {
+
 		Habitacion habitacion = null;
 		boolean habitacionExiste = false;
-		
+
 		habitacionExiste = verificarHabitacionExiste(id);
-		
-		if(habitacionExiste) {
-			throw new Exception("la habitacion "+ id + "no se ha podido crear debido a que ya existe");
-		}else {
-			habitacion=  new Habitacion();
+
+		if (habitacionExiste) {
+			throw new Exception("la habitacion " + id + "no se ha podido crear debido a que ya existe");
+		} else {
+			habitacion = new Habitacion();
 			habitacion.setId(id);
 			habitacion.setListaCamas(listaCamas);
 			habitacion.setEstado(estado);
 			habitacion.setDisponibilidad(disponibilidad);
 			habitacion.setTipoHabticacion(tipoHabitacion);
-			
+
 			listaHabitaciones.add(habitacion);
 		}
 		return habitacion;
-		
+
 	}
-	
-	
+
 	public Habitacion obtenerHabitacion(String id) {
-		
+
 		Habitacion habitacionEncontada = null;
-		if(id!=null) {
+		if (id != null) {
 			for (Habitacion habitacion : listaHabitaciones) {
-				if(habitacion.getId().equals(id)) habitacionEncontada = habitacion;
+				if (habitacion.getId().equals(id))
+					habitacionEncontada = habitacion;
 			}
-			
+
 		}
 		return habitacionEncontada;
-		
+
 	}
-	
+
 	public void updateHabitacion(String id, Habitacion newHabitacion) {
-		
-		
+
 		Habitacion habitacionEncontada = obtenerHabitacion(id);
-		if(habitacionEncontada != null) {
+		if (habitacionEncontada != null) {
 			habitacionEncontada.setId(newHabitacion.getId());
 			habitacionEncontada.setListaCamas(newHabitacion.getListaCamas());
 			habitacionEncontada.setDisponibilidad(newHabitacion.getDisponibilidad());
 			habitacionEncontada.setEstado(newHabitacion.getEstado());
 			habitacionEncontada.setTipoHabticacion(newHabitacion.getTipoHabticacion());
 		}
-		
+
 	}
-	
-public void deleteHabitacion(String id) {
-		
-		if(id!=null) {
+
+	public void deleteHabitacion(String id) {
+
+		if (id != null) {
 			for (Habitacion habitacion : listaHabitaciones) {
-				if(habitacion.getId().equals(id)) listaHabitaciones.remove(habitacion);
+				if (habitacion.getId().equals(id))
+					listaHabitaciones.remove(habitacion);
 				System.out.println("habitacion eliminada correctamente");
 			}
+
+		}
+
+	}
+
+	public boolean verificarHabitacionExiste(String id) {
+		for (Habitacion habitacion : listaHabitaciones) {
+			if (habitacion.getId().equals(id)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	
+	//--------------------FIN-----------------------------
+	
+	
+	//------------------CRUD Reserva------------------------------
+	
+	public Reserva crearReserva(String id, Usuario usuario, Habitacion habitacion, Factura factura) throws ReservaException {
+		
+		Reserva reserva = null;
+		boolean reservaExiste = false;
+		
+		reservaExiste = verificarReservaExiste(id);
+		
+		if(reservaExiste == true) {
 			
+    		alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Reserva no creada");
+			alert.setHeaderText("La reserva con el id # " + id + " no ha sido creada porque ya existe");
+			
+			dialogPane = alert.getDialogPane();
+			dialogPane.getStylesheets().add(getClass().getResource("../resources/Styles.css").toString());
+			dialogPane.getStyleClass().add("dialogs");
+
+			alert.showAndWait();
+			
+			throw new ReservaException("La reserva con el id # " + id + " no se puede guardar porque ya existe");
+			
+
+		} else {
+			reserva = new Reserva();
+			reserva.setId(id);
+			reserva.setUsuario(usuario);
+			reserva.setHabitacion(habitacion);
+			getListaReservas().add(reserva);
 		}
 		
-		
+		return reserva;
 	}
-	
-	
-public boolean verificarHabitacionExiste(String id) {
-	for (Habitacion habitacion : listaHabitaciones) {
-		if(habitacion.getId().equals(id)) {
-			return true;
+
+	private boolean verificarReservaExiste(String id) {
+
+		for (Reserva reserva : getListaReservas()) {
+			if(reserva.getId().equals(id)) {
+				return true;
+			}
 		}
+		
+		return false;
 	}
 	
-	return false;
+	public Reserva obtenerReserva(String id) {
+
+		Reserva reservaEncontrada = null;
+		if (id != null) {
+			for (Reserva reserva : getListaReservas()) {
+				if (reserva.getId().equals(id))
+					reservaEncontrada = reserva;
+			}
+
+		}
+		return reservaEncontrada;
+
 	}
 
+	public void updateReserva(String id, Reserva newReserva) {
 
+		Reserva reservaEncontrada = obtenerReserva(id);
+		if (reservaEncontrada != null) {
+			reservaEncontrada.setId(newReserva.getId());
+			reservaEncontrada.setHabitacion(newReserva.getHabitacion());
+			reservaEncontrada.setUsuario(newReserva.getUsuario());
+			reservaEncontrada.setFactura(newReserva.getFactura());
+		}
+
+	}
+
+	public void deleteReserva(String id) {
+
+		if (id != null) {
+			for (Reserva reserva : getListaReservas()) {
+				if (reserva.getId().equals(id))
+					getListaReservas().remove(reserva);
+				System.out.println("Reserva eliminada correctamente");
+			}
+
+		}
+
+	}
+	
+	
+	//------------------FIN-----------------------------------------
+	
 }
